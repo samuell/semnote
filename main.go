a7888e7a55b0c5c81ac49e3f689c39c27fcec344
Samuel Lampa <samuel.lampa@rilnet.com>
2024-07-27 18:37:38 +0200

    Add regex matching

--- main.go
+++ main.go
@@ -2,18 +2,31 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"path/filepath"
+	"regexp"
 )
 
+var factPatStr = "\\[(.*)\\] \\[(.*)\\] \\[(.*)\\]"
+
 func main() {
+	factPat := regexp.MustCompile(factPatStr)
+
 	matches, err := filepath.Glob("*.md")
 	check(err)
 	if len(matches) == 0 {
 		fmt.Println("No matches!")
 	}
-	for _, m := range matches {
-		fmt.Printf("Match: %s\n", m)
+	for _, mdfile := range matches {
+		//fmt.Printf("Match: %s\n", mdfile)
+		mdtext, err := os.ReadFile(mdfile)
+		facts := factPat.FindAllString(string(mdtext), -1)
+		for _, fact := range facts {
+			fmt.Printf("Fact: %s\n", fact)
+		}
+		check(err)
 	}
+
 }
 
 func check(err error) {
